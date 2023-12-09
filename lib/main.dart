c149d10b38dd77800d9082502d72593758523524
Kiselev Danil <dabikis2855@gmail.com>
2023-12-09 19:51:21 +0600

    [+] add user profile

--- lib/main.dart
+++ lib/main.dart
@@ -71,11 +71,9 @@ class MyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [TopWidget()],
+      children: [ TopWidget()],
     );
   }
 }
-
-
 
 
